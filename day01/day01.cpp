acbbe810a73adf8f1feacdd3b55b6314982e5fdc
Marek S. Lukasiewicz <marek@lukasiewicz.tech>
2023-12-02 00:23:48 +0100

    Open file

--- day01/day01.cpp
+++ day01/day01.cpp
@@ -1,6 +1,27 @@
+#include <fstream>
 #include <iostream>
+#include <string>
 
-int main() {
-    std::cout << 42 << std::endl;
+using namespace std::string_literals;
+
+int main(int argc, char *argv[]) {
+    const auto input_filename = [&] {
+        if (argc >= 2)
+            return std::string{argv[1]};
+        else
+            return "example.txt"s;
+    }();
+
+    std::ifstream input_file{input_filename};
+    if (input_file.fail()) {
+        std::cerr << "Could not open " << input_filename << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    std::string line;
+    while (std::getline(input_file, line)) {
+        std::cout << line << std::endl;
+    }
+
     return EXIT_SUCCESS;
 }
