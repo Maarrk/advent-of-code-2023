eee5a4c81257791b9606a29d0f72440457cbbcc4
Marek S. Lukasiewicz <marek@lukasiewicz.tech>
2023-12-03 11:18:12 +0100

    Split string_view with iterators

--- day02/day02.cpp
+++ day02/day02.cpp
@@ -1,4 +1,5 @@
 #include <charconv>
+#include <format>
 #include <stdexcept>
 #include <string>
 #include <string_view>
@@ -8,6 +9,14 @@
 #include "day02.h"
 
 using namespace std::literals;
+
+struct CubeSet {
+    int red{0};
+    int green{0};
+    int blue{0};
+
+    bool operator==(const CubeSet &) const = default;
+};
 
 int parse_game_id(std::string_view line) {
     if (!line.starts_with("Game "sv)) {
@@ -35,4 +44,55 @@ TEST_CASE("parsing game id") {
     CHECK_THROWS(parse_game_id("Game 1a: 3 blue, 4 red"));
     CHECK_EQ(parse_game_id("Game 1: 3 blue, 4 red"), 1);
     CHECK_EQ(parse_game_id("Game 15: 3 blue, 4 red"), 15);
+}
+
+CubeSet parse_draw(std::string_view draw) {
+    CubeSet set{};
+    struct {
+        int &field;
+        std::string name;
+        bool is_set;
+    } field_mappings[] = {
+        {set.red, " red", false},
+        {set.green, " green", false},
+        {set.blue, " blue", false},
+    };
+
+    auto part_begin = draw.begin();
+    auto part_end = draw.begin();
+    while (part_begin != draw.end()) {
+        part_end = std::find(part_end, draw.end(), ',');
+        std::string_view draw_part{part_begin, part_end};
+
+        for (auto &fm : field_mappings) {
+            if (draw_part.ends_with(fm.name)) {
+                if (fm.is_set) {
+                    throw std::invalid_argument{
+                        std::format("Repeated color{}", fm.name)};
+                }
+                size_t number_end = draw_part.size() - fm.name.size();
+                auto parse_result = std::from_chars(
+                    &draw_part[1], &draw_part[number_end], fm.field);
+                if (parse_result.ec != std::errc{} ||
+                    parse_result.ptr != &draw_part[number_end]) {
+                    throw std::invalid_argument{std::format(
+                        "Invalid dice number for color{} in part", fm.name)};
+                }
+                fm.is_set = true;
+            }
+        }
+
+        if (part_end != draw.end())
+            part_end++; // skip the separator on every iteration except last
+        part_begin = part_end;
+    }
+
+    return set;
+}
+
+TEST_CASE("parsing single draw") {
+    CHECK_EQ(parse_draw(""), CubeSet{});
+    CHECK_EQ(parse_draw(" 1 red"), CubeSet{1, 0, 0});
+    CHECK_EQ(parse_draw(" 1 red, 2 green, 3 blue"), CubeSet{1, 2, 3});
+    CHECK_THROWS(parse_draw(" 1 red, 2 green, 3 red"));
 }
