e87c15af066bb449c9a64c71d99f1d3cf347d9cf
Marek S. Lukasiewicz <marek@lukasiewicz.tech>
2023-12-05 00:11:18 +0100

    WIP: Day 01 part 2

--- day01/day01.cpp
+++ day01/day01.cpp
@@ -5,6 +5,8 @@
 
 using namespace std::string_literals;
 
+void mark_spelled_digits(std::string &text);
+
 int main(int argc, char *argv[]) {
     const auto input_filename = [&] {
         if (argc >= 2)
@@ -21,8 +23,27 @@ int main(int argc, char *argv[]) {
 
     std::string line;
     int sum_part1 = 0;
+    int sum_part2 = 0;
     while (std::getline(input_file, line)) {
+        {
+            auto is_digit = [](char c) { return c >= '0' && c <= '9'; };
+            auto first_it = std::find_if(line.begin(), line.end(), is_digit);
+            if (first_it == line.end()) {
+                std::cerr << "Line " << line << " did not contain any digits"
+                          << std::endl;
+                goto part2; // FIXME: Rework to alternatively run first or
+                            // second part
+            }
+            auto last_it = std::find_if(line.rbegin(), line.rend(), is_digit);
+
+            auto tens = *first_it - '0';
+            auto ones = *last_it - '0';
+
+            sum_part1 += tens * 10 + ones;
+        }
+    part2: {
         auto is_digit = [](char c) { return c >= '0' && c <= '9'; };
+        mark_spelled_digits(line);
         auto first_it = std::find_if(line.begin(), line.end(), is_digit);
         if (first_it == line.end()) {
             std::cerr << "Line " << line << " did not contain any digits"
@@ -34,10 +55,27 @@ int main(int argc, char *argv[]) {
         auto tens = *first_it - '0';
         auto ones = *last_it - '0';
 
-        sum_part1 += tens * 10 + ones;
+        sum_part2 += tens * 10 + ones;
+    }
     }
 
-    std::cout << "Part 1 answer: " << sum_part1 << std::endl;
+    std::cout << "Part 1 answer: " << sum_part1 << std::endl
+              << "Part 2 answer: " << sum_part2 << std::endl;
 
     return EXIT_SUCCESS;
+}
+
+// FIXME: replace words in order they appear in string, not by value
+void mark_spelled_digits(std::string &text) {
+    const char spellings[10][6] = {"zero", // skip when replacing
+                                   "one",  "two",   "three", "four", "five",
+                                   "six",  "seven", "eight", "nine"};
+    for (char i = 1; i < 10; i++) {
+        size_t match_start = text.find(spellings[i]);
+        if (match_start != -1)
+            text[match_start] = '0' + i;
+        match_start = text.rfind(spellings[i]);
+        if (match_start != -1)
+            text[match_start] = '0' + i;
+    }
 }
