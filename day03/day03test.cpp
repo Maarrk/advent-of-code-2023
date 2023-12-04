cf52c43989ddfa01d0135e0b9bbe5949b0a017c5
Marek S. Lukasiewicz <marek@lukasiewicz.tech>
2023-12-04 11:58:52 +0100

    Change build to clang

--- day03/day03test.cpp
+++ day03/day03test.cpp
@@ -19,6 +19,7 @@ TEST_CASE("mdspan basics") {
     CHECK_EQ(mds(2, 0), 7);
 
     auto top = std::submdspan(mds, 0, std::full_extent);
+    CHECK_EQ(top.rank(), 1);
     CHECK_EQ(top.extent(0), 3);
     CHECK_EQ(top.extent(1), 3);
     CHECK_EQ(top.extent(2), 3);
@@ -27,7 +28,7 @@ TEST_CASE("mdspan basics") {
     CHECK_EQ(top(1), 2);
     CHECK_EQ(top(2), 3);
 
-    // // Doesn't compile:
+    // // Doesn't compile (tried on MSVC and clang):
     // auto center = std::submdspan(mds, std::full_extent, 1);
     // CHECK_EQ(center.size(), 3);
     // CHECK_EQ(center(0), 2);
@@ -38,10 +39,13 @@ TEST_CASE("mdspan basics") {
     for (auto &&n : input) {
         input_v.push_back(n);
     }
-    auto mda = stdex::mdarray<int, std::dextents<int, 2>>{stdex::extents{3, 3},
-                                                          input_v};
-    CHECK_EQ(mds(0, 0), 1);
-    CHECK_EQ(mds(2, 0), 7);
+    auto mda = stdex::mdarray<int, std::dextents<int, 2>>{
+        std::extents<int, 3, 3>{}, input_v};
+    CHECK_EQ(mda(0, 0), 1);
+    CHECK_EQ(mda(2, 0), 7);
+    CHECK_EQ(mda.extent(0), 3);
+    CHECK_EQ(mda.extent(1), 3);
+    CHECK_EQ(mda.rank(), 2);
 }
 
 TEST_CASE("mdspan with string") {
@@ -52,4 +56,6 @@ TEST_CASE("mdspan with string") {
     CHECK_EQ(mds(0, 0), 'a');
     CHECK_EQ(mds(1, 1), 'e');
     CHECK_EQ(mds(0, 2), 'c');
+    CHECK_EQ(mds.extent(0), 2);
+    CHECK_EQ(mds.extent(1), 3);
 }
